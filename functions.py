794029e0a46d25eaee7aec5bdf983eb0a87a905c
kpavia <kennethpavia@gmail.com>
2018-05-08 11:44:53 -0600

    added functions.py

---
+++ functions.py
@@ -0,0 +1,9 @@
+"""
+These are the completed functions. This .py file is temporary while I work on functions and complete them.
+"""
+
+
+def print_verb_dict(verb):
+    """Prints out the info in the verb's dictionary"""
+    for keys in verb:
+        print(f'{keys}: {verb[keys]}')
