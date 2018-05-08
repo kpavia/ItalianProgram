9cc4a42f52f2448dc1dccd4f8cade39cbbf73650
kpavia <kennethpavia@gmail.com>
2018-05-08 11:52:16 -0600

    updated functions.py with more functions

--- functions.py
+++ functions.py
@@ -7,3 +7,43 @@ def print_verb_dict(verb):
     """Prints out the info in the verb's dictionary"""
     for keys in verb:
         print(f'{keys}: {verb[keys]}')
+
+
+def verb_ending(verb):
+    """
+    Returns the last 3 characters of the verb to determine the verb type
+    Verb is passed in as a string
+    Error handling checks to make sure the verb is a string data type and that it's actually a verb
+    and not some other word
+    Returns: a boolean False if an error is found
+    """
+    try:
+        is_verb = verb[-3:]
+        if is_verb in ["are", "ere", "ire"]:
+            return is_verb
+        else:
+            return False
+    except TypeError:
+        raise TypeError
+
+
+def strip_off_ending(verb, tense):
+    """
+    Strips the ending (last 3 characters) off the verb
+    Parameters: accepts a string
+    Return: returns a string
+    Error handling: checks to make sure parameter is a verb and not just a verb
+    Returns an error message if that condition is true
+    """
+    ending = verb_ending(verb)
+    if ending is False:
+        raise ValueError
+    else:
+        try:
+            tenses = ["io", "lei", "lui", "voi", "loro"]
+            if verb[-4] == "i" and tense not in tenses:
+                return verb[:-4]
+            else:
+                return verb[:-3]
+        except TypeError:
+            raise TypeError
