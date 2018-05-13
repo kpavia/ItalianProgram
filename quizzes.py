fb866f42e6b012e2e761af72e33397b48a940332
Kenneth Pavia <kennethpavia@gmail.com>
2018-05-13 16:21:02 -0600

    added present -ere quiz

--- quizzes.py
+++ quizzes.py
@@ -50,8 +50,50 @@ def begin_present_are_quiz():
             verb_good = False
 
 
+def begin_present_ere_quiz():
+    print("Get ready for a quiz.\nInstructions: You'll be shown a verb and a pronoun. Conjugate it in the present tense"
+          ".\n")
+    verb_good = False
+    verb = ""
+    pronoun = ""
+    go_again = True
+    while go_again:
+        while not verb_good:
+            verb = random.choice(ere_verb_options)
+            checker = functions.verb_ending_good(verb)
+            if checker is True:
+                verb_good = True
+        pronoun_good = False
+        while not pronoun_good:
+            pronoun = random.choice(pronouns)
+            if pronoun in pronouns:
+                pronoun_good = True
+        ending = functions.verb_ending(verb)
+        if ending == "ere":
+            answer = ere_present_quiz(verb, pronoun)
+            checker = input(f'Tense: Presente \nVerb: {verb} \nPronoun: {pronoun}...\n')
+            if answer == checker:
+                print("Correct!")
+            else:
+                print(f'Incorrect.\nCorrect answer is {answer}')
+        another = input("Go again? y/n\n").lower()
+        if another != "y":
+            go_again = False
+            print("Quiz over.")
+        else:
+            verb_good = False
+
+
 def are_present_quiz(verb, pronoun):
     return functions.conjugate_present_are_verb(verb, pronoun, "presente")
+
+
+def ere_present_quiz(verb, pronoun):
+    return functions.conjugate_present_ere_verb(verb, pronoun, "presente")
+
+
+def ire_present_quiz(verb, pronoun):
+    return functions.conjugate_present_ire_verb(verb, pronoun, "presente")
 
 
 def begin_imperfect_are_quiz():
@@ -93,4 +135,4 @@ def are_imperfect_quiz(verb, pronoun):
     return functions.conjugate_imperfect_are_verb(verb, pronoun, "imperfetto")
 
 
-begin_imperfect_are_quiz()
+begin_present_ere_quiz()
